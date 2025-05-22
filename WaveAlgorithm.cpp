bf140a299d8e1ed80d0f91c0a2f92a0043d821fe
Pan_Sasha <pankov.aleksander05@gmail.com>
2025-05-22 21:28:43 +0700

    Update WaveAlgorithm.cpp

--- WaveAlgorithm.cpp
+++ WaveAlgorithm.cpp
@@ -1,53 +1,8 @@
 #include "WaveAlgorithm.h"
 
-MyMap MapReader::readMapFromFile(const std::string& filePath) {
-    std::vector<char> grid;
-    std::ifstream file(filePath);
-    if (!file.is_open()) {
-        std::cerr << "Unable to open file." << std::endl;
-        return MyMap({}, 0, 0);
-    }
-    std::string line;
-    int mapHeight = 0;
-    while (std::getline(file, line)) {
-        for (char value : line) {
-            if (value != ' ') {
-                grid.push_back(value);
-            }
-        }
-        ++mapHeight;
-    }
-
-    int mapWeight = grid.size() / mapHeight;
-    file.close();
-
-    return MyMap(grid, mapWeight, mapHeight);
-};
-
-void MapReader::writeMapToFile(const std::string& filePath, const MyMap& map) {
-    std::ofstream outFile(filePath);
-    if (!outFile) {
-        std::cerr << "Ошибка при открытии файла для записи!" << std::endl;
-        return;
-    }
-    const auto& grid = map.getGrid();
-    int mapHeight = map.getMapHeight();
-    int mapWeight = map.getMapWeight();
-
-    for (int i = 0; i < mapHeight; ++i) {
-        for (int j = 0; j < mapWeight; ++j) {
-            outFile << grid[i * mapWeight + j] << " ";
-        }
-        outFile << std::endl;
-    }
-
-    outFile.close();
-};
-
-
 MyMap::MyMap(const std::vector<char>& grid, int mapWeight, int mapHeight): grid(grid), mapWeight(mapWeight), mapHeight(mapHeight){};
 
-MyMap::MyMap(const MyMap& other): grid(other.grid), mapWeight(other.mapWeight), mapHeight(other.mapHeight) {};
+MyMap::MyMap(const MyMap& other): grid(other.grid), mapWeight(other.mapWeight), mapHeight(other.mapHeight) {}
 
 int MyMap::findHero() const{
     for (int y = 0; y < mapHeight; ++y) {
@@ -80,10 +35,6 @@ int MyMap::getMapHeight() const {
     return mapHeight;
 }
 
-const std::vector<char>& MyMap::getGrid() const {
-    return grid;
-}
-
 bool MyMap::isWalkable(int x, int y ) const {
     if (x < 0 || x >= mapWeight || y < 0 || y >= mapHeight) {
         return false;
@@ -97,8 +48,7 @@ void MyMap::waveAlgorithm() {
     int exitPos = findExit();
 
     if (heroPos == -1 || exitPos == -1) {
-        std::cout << "Hero or exit not found!" << std::endl;
-        return;
+        throw std::runtime_error("No hero or exit found");
     }
 
     std::queue<int> q;
@@ -138,5 +88,4 @@ void MyMap::waveAlgorithm() {
             }
         }
     }
-    return;
 };
