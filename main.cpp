1e4166a21603c459153c767b6cfa0e0a1b539aa3
Pan_Sasha <pankov.aleksander05@gmail.com>
2025-05-22 21:29:24 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "WaveAlgorithm.h"
-
+#include "ReadWriteFile.h"
 int main() {
-    MyMap myMap = MapReader::readMapFromFile("/Users/mac_aleksandr/mymap.txt");
+    MyMap myMap(MapReader::readMapFromFile("/Users/mac_aleksandr/mymap.txt"));
     myMap.waveAlgorithm();
     MapReader::writeMapToFile("updated_map.txt", myMap);
-
+    MapReader::readMapFromFile("updated_map.txt");
 }
